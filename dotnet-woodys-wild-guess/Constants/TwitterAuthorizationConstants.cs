b401418bd5bc6194d8ea471cfbb0778feae2ab65
Bobby <bobbielite@pm.me>
2024-10-09 23:53:15 -0400

    Adding Twitter Support

---
+++ dotnet-woodys-wild-guess/Constants/TwitterAuthorizationConstants.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace dotnet.woodyswildguess.Constants;
+
+public class TwitterAuthorizationConstants
+{
+    public const string GrantType = "authorization_code";
+    public const string CodeVerifier = "challenge";
+}
